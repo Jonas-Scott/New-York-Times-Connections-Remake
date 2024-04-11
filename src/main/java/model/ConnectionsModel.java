8d5d30ea4c9f8afba9c92f453280bf9501ea0799
Owen Reilly <opr002@bucknell.edu>
2024-04-11 16:08:57 -0400

    Skeleton for classes

--- src/main/java/model/ConnectionsModel.java
+++ src/main/java/model/ConnectionsModel.java
@@ -18,5 +18,51 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 public class ConnectionsModel {
+
+
+    /**
+     * Array of all possible levels to chose from
+     * Can be easy, medium, hard
+     */
+    private ArrayList<String> levels;
+
+
+    /**
+     * Kind of like a state-check
+     * Determines weather or not we are in game
+     * Will probably need a getter
+     */
+    private boolean inGame;
+
+    /**
+     * Tracks how many guesses we have.
+     */
+    private int guesses;
+
+
+    /**
+     * Shouldn't have more than 4 guesses but we can change it if we need to
+     */
+    private final int maxGuesses = 4;
+
+    /**
+     *
+     */
+    public void chooseLevel(){
+
+    }
+
+
+    public void enterGame(){
+
+    }
+
+
+    private void reset(){
+
+    }
+
 }
