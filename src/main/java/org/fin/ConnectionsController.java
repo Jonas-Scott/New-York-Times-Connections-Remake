ad8ba233ef03803de7c17b3fb0428a971ab8f0da
Jonas Scott <jks030@bucknell.edu>
2024-04-12 15:49:18 -0400

    starting view

--- src/main/java/org/fin/ConnectionsController.java
+++ src/main/java/org/fin/ConnectionsController.java
@@ -1,16 +1,22 @@
+/* *****************************************
+ * CSCI 205 - Software Engineering and Design
+ * Spring 2024
+ * Instructor: Prof. Lily Romano
+ *
+ * Name: Casey King
+ * Section: 1:00
+ * Date: 4/10/24
+ * Time: 3:51 PM
+ *
+ * Project: csci205_final_project
+ * Package: org.fin
+ * Class: ConnectionsMain
+ *
+ * Description:
+ *
+ * ****************************************
+ */
 package org.fin;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class ConnectionsController extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
+public class ConnectionsController {
 }
--- src/main/java/view/ConnectionsView.java
+++ src/main/java/view/ConnectionsView.java
@@ -18,7 +18,11 @@
  */
 package view;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import model.ConnectionsModel;
 
 public class ConnectionsView {
@@ -26,8 +30,47 @@ public class ConnectionsView {
     /** Model object for the ConnectionsModel Class */
     private ConnectionsModel theModel;
 
-    /** Root object for the JavaFX class */
-    private TilePane root;
+    /** Root object for the Connection game screen */
+    private TilePane gamePlayRoot;
+
+    /** Root object for the home screen */
+    private VBox homeScreenRoot;
+
+    /**
+     * Constructor for ConnectionsView class
+     * @author - Jonas Scott
+     */
+    public ConnectionsView(ConnectionsModel theModel) {
+        this.theModel = theModel;
+
+        initSceneGraph();
+        initStyling();
+
+    }
+
+    /**
+     * Init method to initialize all objects in the scene graph
+     * @author - Jonas Scott
+     */
+    private void initSceneGraph() {
+        this.homeScreenRoot = new VBox();
+        this.homeScreenRoot.setAlignment(Pos.CENTER);
+
+        this.homeScreenRoot.getChildren().add(new Text("Welcome to Connections \n Select your Difficulty"));
+        this.homeScreenRoot.getChildren().add(new Button("Easy"));
+        this.homeScreenRoot.getChildren().add(new Button("Medium"));
+        this.homeScreenRoot.getChildren().add(new Button("Hard"));
+        this.homeScreenRoot.getChildren().add(new Button("Extreme"));
+    }
+
+    // This might be completely unnecessary we could maybe delete it since we are styling in a CSS file anyway
+    private void initStyling() {
+    }
+
+    /** Getter for the root of the Home Screen */
+    public VBox getHomeScreenRoot() {
+        return homeScreenRoot;
+    }
 
 
 }
