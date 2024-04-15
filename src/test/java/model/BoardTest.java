8694eb40bec874caab0fb8958f3dafa502d8d96c
Owen Reilly <opr002@bucknell.edu>
2024-04-15 13:16:41 -0400

    Skeleton for classes

---
+++ src/test/java/model/BoardTest.java
@@ -0,0 +1,40 @@
+package model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @BeforeEach
+    void setUp() {
+        Board board = new Board()
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void select() {
+
+    }
+
+    @Test
+    void checkSelected() {
+    }
+
+    @Test
+    void adjustBoard() {
+    }
+
+    @Test
+    void shuffleBoard() {
+    }
+
+    @Test
+    void getNumSelected() {
+    }
+}
